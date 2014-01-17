a050cbfb6c42a8f838c824db279ac4c23cb89e4d
Marius Kintel <marius@kintel.net>
2014-01-16 23:05:07 -0500

    Filter files through things.txt

--- find_scad_files.sh
+++ find_scad_files.sh
@@ -2,5 +2,14 @@
 
 rm -f scadfiles.txt
 for f in unpacked/*; do
-  find -s $f -name "*.scad" -print -quit >> scadfiles.txt
+  # folder/<THINGID>
+  [[ $f =~ ^[^\/]+/([[:digit:]]+) ]]
+  THINGID=${BASH_REMATCH[1]}
+
+  if [[ `cat things.txt` =~ (^|[[:space:]])"$THINGID"($|[[:space:]]) ]]; then
+    find -s $f -name "*.scad" -print -quit >> scadfiles.txt
+  else
+    echo "Warning: Thing $THINGID not in things.txt"
+  fi
+
 done
