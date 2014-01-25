fd4b650696c7e11d12be3bb6631c91708ea9a5b2
Marius Kintel <marius@kintel.net>
2014-01-25 12:06:12 -0500

    Added python script for creating csv including performance calculations

---
+++ createcsv.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import csv
+
+# Read popularity database
+jobreader = csv.reader(open('customizer-jobs.csv', 'rb'))
+jobreader.next() # Skipe header
+jobdict = {int(k): int(v) for k, v in jobreader}
+
+ifile  = open('times.csv', "rb")
+ofile  = open('comparison.csv', "wb")
+reader = csv.reader(ifile)
+writer = csv.writer(ofile, delimiter=',')
+
+rownum = 0
+for row in reader:
+    # Save header row.
+    if rownum == 0:
+        header = row
+        header.append('Improvement from 2013.06')
+        header.append('Improvement from master')
+        header.append('hide')
+        header.append('popularity')
+        writer.writerow(row)
+    else:
+        thingid = row[0] = int(row[0])
+        oldtime = float(row[1])
+        mastertime = float(row[2])
+        newtime = float(row[3])
+        changed = row[4] = int(row[4]) != 0
+        improvement = (oldtime+0.1)/(newtime+0.1)
+        row.append('{:.1f}'.format(improvement))
+        row.append('{:.1f}'.format((mastertime+0.1)/(newtime+0.1)))
+        row.append(not changed and
+                   ((oldtime < 1 and mastertime < 1 and newtime < 1) or
+                    (improvement >= 0.8 and improvement <= 1.2)))
+        if thingid in jobdict: row.append(jobdict[thingid])
+        else: row.append(-1)
+        writer.writerow(row)
+#        colnum = 0
+#        for col in row:
+#            print '%-8s: %s' % (header[colnum], col)
+#            colnum += 1
+            
+    rownum += 1
+
+ifile.close()
+ofile.close()
